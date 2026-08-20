1b6d6ba69f435305a2497dd69b6a62c4e544efc6
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-08-20 12:54:52 +0100

    refactor(concourse): use iam role instead of user

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -1,20 +1,11 @@
 set -euo pipefail
 
-aws_account_id=$(echo "$secrets" | jq -r .aws_account_id)
-aws_access_key_id=$(echo "$secrets" | jq -r .aws_access_key_id)
-
-aws_secret_access_key=$(echo "$secrets" | jq -r .aws_secret_access_key)
 domain=$(echo "$secrets" | jq -r .domain)
-
 service_subdomain=$(echo "$secrets" | jq -r .service_subdomain)
 ecr_repository=$(echo "$secrets" | jq -r .ecr_repository)
 azure_secret_name=$(echo "$secrets" | jq -r .azure_secret_name)
-aws_account_name=$(echo "$secrets" | jq -r .domain)
-
+aws_account_name=$(echo "$secrets" | jq -r .aws_account_name)
 branch_name=$branch
-
-export AWS_ACCESS_KEY_ID=$aws_access_key_id
-export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
 
 git config --global url."https://x-access-token:$github_access_token@github.com/".insteadOf "https://github.com/"
 
@@ -26,9 +17,6 @@ cd resource-repo/terraform/storage
 
 terraform init -backend-config=env/${env}/backend-${env}.tfbackend -reconfigure
 terraform apply \
--var "aws_account_id=$aws_account_id" \
--var "aws_access_key_id=$aws_access_key_id" \
--var "aws_secret_access_key=$aws_secret_access_key" \
 -var "domain=$domain" \
 -auto-approve
 
@@ -36,9 +24,6 @@ cd ../lambda
 
 terraform init -backend-config=env/${env}/backend-${env}.tfbackend -reconfigure
 terraform apply \
--var "aws_account_id=$aws_account_id" \
--var "aws_access_key_id=$aws_access_key_id" \
--var "aws_secret_access_key=$aws_secret_access_key" \
 -var "domain=$domain" \
 -var "service_subdomain=$service_subdomain" \
 -var "container_ver=${tag}" \
@@ -52,8 +37,5 @@ cd ../api_gateway
 
 terraform init -backend-config=env/${env}/backend-${env}.tfbackend -reconfigure
 terraform apply \
--var "aws_account_id=$aws_account_id" \
--var "aws_access_key_id=$aws_access_key_id" \
--var "aws_secret_access_key=$aws_secret_access_key" \
 -var "domain=$domain" \
 -auto-approve
